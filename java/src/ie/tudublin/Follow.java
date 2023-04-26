2d36bcf5153c9d122edf62c2e31f9edfbbdc2090
C21522836 <C21522836@mytudublin.ie>
2023-04-26 16:50:24 +0100

    implemented follow class

---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,40 @@
+package ie.tudublin;
+
+//Follow class
+public class Follow 
+{
+    //initializing word and count fields
+    private String word;
+    private int count;
+
+    //increment word count
+    public void increment()
+    {
+        this.count++;
+    }
+
+    //method to pull the word 
+    public String accessWord()
+    {
+        return this.word;
+    }
+
+    //method to pull the word count
+    public int accessCount()
+    {
+        return this.count;
+    }
+
+    //constructor to initialize the word and start the counter at 1
+    public Follow(String word)
+    {
+        this.word = word;
+        this.count = 1;
+    }
+
+    //follow object into a string
+    public String toString()
+    {
+        return this.word + "(" + this.count + ")";
+    }
+}
